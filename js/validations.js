e46d3d785be152e06ab4bb886ed66c6ad05530ba
andrespienizzio <andrespienizzio@gmail.com>
2021-10-18 04:49:27 -0300

    Add email validation js

--- js/validations.js
+++ js/validations.js
@@ -1,26 +1,24 @@
-
-var valid = []
-var invalid = []
+const valid = []
+const invalid = []
 
 // Full Name
  
 var fullName = document.getElementById('fullName');
 var fullNameError = document.getElementById('fullNameError');
-fullName.addEventListener('blur', fullNameBlur);
 
+fullName.addEventListener('blur', fullNameBlur);
 function fullNameBlur() {    
-    var x = fullName.value
-    if (x.length < 7 || x.indexOf(' ') <= 0 || x.indexOf(' ') >= x.length) {        
+    let x = fullName.value;
+    if (x.length < 6 || x.indexOf(' ') <= 1) {        
         fullNameError.style.display = "block";
         fullNameError.style.display = "visible";
         invalid[0] = fullNameError.textContent;
-        return false;
     } else {
         fullNameError.style.display = "0";
         valid[0] = "Full Name: " + x + '\n';
-        return true;
     }
 }
+
 fullName.addEventListener("focus", fullNameFocus);
 function fullNameFocus() {
     fullNameError.style.display = "none";
@@ -28,9 +26,49 @@ function fullNameFocus() {
 
 // Email
 
+var email = document.getElementById('email');
+var emailError = document.getElementById('emailError');
+
+email.addEventListener('blur', emailBlur);
+function emailBlur() {    
+    let x = email.value;
+    if (x.indexOf('@') == -1 || x.indexOf('.') == -1 || x.includes(' ') || x.indexOf('.') == email.length - 1){        
+        emailError.style.display = "block";
+        emailError.style.display = "visible";
+        invalid[1] = emailError.textContent;
+    } else {
+        emailError.style.display = "0";
+        valid[1] = "Email: " + x + '\n';
+    }
+}
+
+email.addEventListener("focus", emailFocus);
+function emailFocus() {
+    emailError.style.display = "none";
+}
 
 // Password
 
+var password = document.getElementById('password');
+var passwordError = document.getElementById('passwordError');
+
+password.addEventListener('blur', passwordBlur);
+function passwordBlur() {    
+    let x = password.value;
+    if (x.indexOf('@') == -1 || x.indexOf('.') == -1 || x.includes(' ') || x.indexOf('.') == password.length - 1){        
+        passwordError.style.display = "block";
+        passwordError.style.display = "visible";
+        invalid[1] = passwordError.textContent;
+    } else {
+        passwordError.style.display = "0";
+        valid[1] = "password: " + x + '\n';
+    }
+}
+
+password.addEventListener("focus", passwordFocus);
+function passwordFocus() {
+    passwordError.style.display = "none";
+}
 
 // Confirm Password
 
