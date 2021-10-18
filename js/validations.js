330ae28ed1320f4165e514e2572c64ee4874a18e
andrespienizzio <andrespienizzio@gmail.com>
2021-10-18 06:53:11 -0300

    Change error message width

--- js/validations.js
+++ js/validations.js
@@ -12,17 +12,17 @@ fullName.addEventListener('blur', fullNameBlur);
 function fullNameBlur() {    
     let x = fullName.value;
     if (x.length < 6 || x.indexOf(' ') <= 1) {        
-        fullNameError.style.display = "block";
-        fullNameError.style.display = "visible";
+        fullNameError.style.display = 'block';
+        fullNameError.style.display = 'visible';
         invalid[0] = fullNameError.textContent;
     } else {
-        valid[0] = "Full Name: " + x + '\n';
+        valid[0] = 'Full Name: ' + x + '\n';
     }
 }
 
-fullName.addEventListener("focus", fullNameFocus);
+fullName.addEventListener('focus', fullNameFocus);
 function fullNameFocus() {
-    fullNameError.style.display = "none";
+    fullNameError.style.display = 'none';
 }
 
 // Email
@@ -34,17 +34,17 @@ email.addEventListener('blur', emailBlur);
 function emailBlur() {    
     let x = email.value;
     if (x.indexOf('@') == -1 || x.indexOf('.') == -1 || x.includes(' ') || x.indexOf('.') == email.length - 1){        
-        emailError.style.display = "block";
-        emailError.style.display = "visible";
+        emailError.style.display = 'block';
+        emailError.style.display = 'visible';
         invalid[1] = emailError.textContent;
     } else {
-        valid[1] = "Email: " + x + '\n';
+        valid[1] = 'Email: ' + x + '\n';
     }
 }
 
-email.addEventListener("focus", emailFocus);
+email.addEventListener('focus', emailFocus);
 function emailFocus() {
-    emailError.style.display = "none";
+    emailError.style.display = 'none';
 }
 
 // Password
@@ -56,17 +56,17 @@ password.addEventListener('blur', passwordBlur);
 function passwordBlur() {    
     let x = password.value;
     if  (x.length < 8 || x.search(/\w/) < 0 || x.search(/\d/) < 0) {
-        passwordError.style.display = "block";
-        passwordError.style.display = "visible";
+        passwordError.style.display = 'block';
+        passwordError.style.display = 'visible';
         invalid[2] = passwordError.textContent;
     } else {
-        valid[2] = "Password: " + x + '\n';
+        valid[2] = 'Password: ' + x + '\n';
     }
 }
 
-password.addEventListener("focus", passwordFocus);
+password.addEventListener('focus', passwordFocus);
 function passwordFocus() {
-    passwordError.style.display = "none";
+    passwordError.style.display = 'none';
 }
 
 // Confirm Password
@@ -78,17 +78,17 @@ confirmPassword.addEventListener('blur', confirmPasswordBlur);
 function confirmPasswordBlur() {    
     let x = confirmPassword.value;
     if  (x !== password.value) {
-        confirmPasswordError.style.display = "block";
-        confirmPasswordError.style.display = "visible";
+        confirmPasswordError.style.display = 'block';
+        confirmPasswordError.style.display = 'visible';
         invalid[3] = confirmPasswordError.textContent;
     } else {
-        valid[3] = "Confirm Password: " + x + '\n';
+        valid[3] = 'Confirm Password: ' + x + '\n';
     }
 }
 
-confirmPassword.addEventListener("focus", confirmPasswordFocus);
+confirmPassword.addEventListener('focus', confirmPasswordFocus);
 function confirmPasswordFocus() {
-    confirmPasswordError.style.display = "none";
+    confirmPasswordError.style.display = 'none';
 }
 
 // Age
@@ -100,17 +100,17 @@ age.addEventListener('blur', ageBlur);
 function ageBlur() {    
     let x = age.value;
     if  (x < 17) {
-        ageError.style.display = "block";
-        ageError.style.display = "visible";
+        ageError.style.display = 'block';
+        ageError.style.display = 'visible';
         invalid[4] = ageError.textContent;
     } else {
-        valid[4] = "Age: " + x + '\n';
+        valid[4] = 'Age: ' + x + '\n';
     }
 }
 
-age.addEventListener("focus", ageFocus);
+age.addEventListener('focus', ageFocus);
 function ageFocus() {
-    ageError.style.display = "none";
+    ageError.style.display = 'none';
 }
 
 // Phone Number
@@ -122,17 +122,17 @@ phone.addEventListener('blur', phoneBlur);
 function phoneBlur() {    
     let x = phone.value;
     if  (x.length < 7 || x.includes('.') || x.includes('-')) {
-        phoneError.style.display = "block";
-        phoneError.style.display = "visible";
+        phoneError.style.display = 'block';
+        phoneError.style.display = 'visible';
         invalid[5] = phoneError.textContent;
     } else {
-        valid[5] = "Phone: " + x + '\n';
+        valid[5] = 'Phone: ' + x + '\n';
     }
 }
 
-phone.addEventListener("focus", phoneFocus);
+phone.addEventListener('focus', phoneFocus);
 function phoneFocus() {
-    phoneError.style.display = "none";
+    phoneError.style.display = 'none';
 }
 
 // Address
@@ -144,17 +144,17 @@ address.addEventListener('blur', addressBlur);
 function addressBlur() {    
     let x = address.value;
     if  (x.length < 6 || x.indexOf(' ') <= 1 || x.search(/\d/) < 0 || x.search(/[a-z]/) < 0) {
-        addressError.style.display = "block";
-        addressError.style.display = "visible";
+        addressError.style.display = 'block';
+        addressError.style.display = 'visible';
         invalid[6] = addressError.textContent;
     } else {
-        valid[6] = "Address: " + x + '\n';
+        valid[6] = 'Address: ' + x + '\n';
     }
 }
 
-address.addEventListener("focus", addressFocus);
+address.addEventListener('focus', addressFocus);
 function addressFocus() {
-    addressError.style.display = "none";
+    addressError.style.display = 'none';
 }
 
 // City
@@ -166,17 +166,17 @@ city.addEventListener('blur', cityBlur);
 function cityBlur() {    
     let x = city.value;
     if  (x.length < 3) {
-        cityError.style.display = "block";
-        cityError.style.display = "visible";
+        cityError.style.display = 'block';
+        cityError.style.display = 'visible';
         invalid[7] = cityError.textContent;
     } else {
-        valid[7] = "City: " + x + '\n';
+        valid[7] = 'City: ' + x + '\n';
     }
 }
 
-city.addEventListener("focus", cityFocus);
+city.addEventListener('focus', cityFocus);
 function cityFocus() {
-    cityError.style.display = "none";
+    cityError.style.display = 'none';
 }
 
 // Postal Code
@@ -188,17 +188,17 @@ postalCode.addEventListener('blur', postalCodeBlur);
 function postalCodeBlur() {    
     let x = postalCode.value;
     if  (x.length < 3) {
-        postalCodeError.style.display = "block";
-        postalCodeError.style.display = "visible";
+        postalCodeError.style.display = 'block';
+        postalCodeError.style.display = 'visible';
         invalid[8] = postalCodeError.textContent;
     } else {
-        valid[8] = "Postal Code: " + x + '\n';
+        valid[8] = 'Postal Code: ' + x + '\n';
     }
 }
 
-postalCode.addEventListener("focus", postalCodeFocus);
+postalCode.addEventListener('focus', postalCodeFocus);
 function postalCodeFocus() {
-    postalCodeError.style.display = "none";
+    postalCodeError.style.display = 'none';
 }
 
 // ID Number
@@ -210,20 +210,27 @@ idNumber.addEventListener('blur', idNumberBlur);
 function idNumberBlur() {    
     let x = idNumber.value;
     if  (x.length < 7 || x.length > 8 || x.includes('.') || x.includes('-')) {
-        idNumberError.style.display = "block";
-        idNumberError.style.display = "visible";
+        idNumberError.style.display = 'block';
+        idNumberError.style.display = 'visible';
         invalid[9] = idNumberError.textContent;
     } else {
-        valid[9] = "Id Number: " + x + '\n';
+        valid[9] = 'Id Number: ' + x + '\n';
     }
 }
 
-idNumber.addEventListener("focus", idNumberFocus);
+idNumber.addEventListener('focus', idNumberFocus);
 function idNumberFocus() {
-    idNumberError.style.display = "none";
+    idNumberError.style.display = 'none';
 }
 
 // Send Button
+
+var sendButton = document.getElementById('sendButton');
+sendButton.addEventListener('click', function(){
+    alert('Hello World!'); });
+
+
+
 
 
 // BONUS
@@ -240,7 +247,7 @@ function idNumberFocus() {
 
 
 
-    var itemInput = document.querySelector('input[type="text"]');
+    var itemInput = document.querySelector('input[type='text']');
 
     itemInput.addEventListener('keydown', runEvent);
 
